8878e7ba277e99e1d8e32e5f5641dfb44c3f9d6d
masramosn-beep <masramosn@gmail.com>
2026-02-09 20:27:28 +0100

    Create firebase-config.js

---
+++ assets/js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+apiKey: "AIzaSyB-PXudwYTh5nmqEe3PjBjXwF70UGbbkd4",
+  authDomain: "volley-pwa.firebaseapp.com",
+  projectId: "volley-pwa",
+  storageBucket: "volley-pwa.firebasestorage.app",
+  messagingSenderId: "514454856982",
+  appId: "1:514454856982:web:33ca4ada6888a9d1829c37",
+  measurementId: "G-J9W202Y1JR"
+  };
